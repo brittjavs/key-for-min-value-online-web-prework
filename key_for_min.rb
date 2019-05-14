d99491b3ce66a0d74ccb6357bb42a89f082e98b9
Brittany Javalera <brittjavalera@gmail.com>
2019-05-14 22:05:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+ if name_hash = {}
+   return nil
+ else
+   name_hash.each do |name, value|
+     if value < first_value
+       
+     end
+ end 
+ end
+ end
