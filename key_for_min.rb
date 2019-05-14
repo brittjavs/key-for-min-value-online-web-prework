9bede4cdec7f5a96a1306ec0bca997f9c36c6f2d
Brittany Javalera <brittjavalera@gmail.com>
2019-05-14 22:23:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
    minimum_key = nil
    minimun_value = nil
    name_hash.each do |name, value|
-     if value < minimun_value
+     if value > minimun_value
        
      end
  end 
